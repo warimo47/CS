45ceae988454a8697c88ec212e69874329c55e8c
KangsanBae <warimo47@naver.com>
2020-07-02 11:28:12 +0900

    Add CS_TCP_Client project Add CS_TCP_Server project

---
+++ CS_TCP_Client/CS_TCP_Client/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+public class TCP_Client
+{
+    public static void Main(string[] arg)
+    {
+        string args0 = "127.0.0.1"; // ip
+        string args1 = "4211"; // 포트 번호
+        string args2 = "Hello"; // 메시지
+
+        TcpClient client = new TcpClient(); // (ip주소 , 포트 번호)
+
+        while(true)
+        {
+            try
+            {
+                client.Connect(args0, Int32.Parse(args1));
+                Console.WriteLine("서버에 연결되었습니다.");
+                break;
+            }
+            catch
+            {
+                Console.WriteLine("서버에 연결이 되지 않습니다. 재시도 중...");
+            }
+        }
+            
+        NetworkStream ns = client.GetStream();
+        StreamWriter writer = new StreamWriter(ns);
+
+        while(true)
+        {
+            Console.Write("보낼 문자열 입력 : ");
+            args2 = Console.ReadLine();
+
+            try
+            {
+                writer.WriteLine(args2);
+                writer.Flush();
+            }
+            catch
+            {
+                Console.WriteLine("서버와 연결이 끊어졌습니다.");
+                break;
+            }
+        }
+    }
+}
---
+++ CS_TCP_Server/CS_TCP_Server/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+public class TCP_Server
+{
+    public static void Main(string[] args)
+    {
+        TcpListener tcp_Listener = new TcpListener(IPAddress.Any, 4211);
+        tcp_Listener.Start();
+
+        Console.WriteLine("[Server] Start");
+
+        TcpClient client = tcp_Listener.AcceptTcpClient(); // 클라이언트와 접속
+
+        Console.WriteLine("[Server] 클라이언트가 연결되었습니다.");
+
+        NetworkStream ns = client.GetStream();
+        StreamReader reader = new StreamReader(ns);
+
+        string msg = "";
+
+        while (true)
+        {
+            try
+            {
+                msg = reader.ReadLine();
+
+                Console.WriteLine("[Client 메시지] : " + msg);
+            }
+            catch
+            {
+                Console.WriteLine("[Client] Disconnected");
+                break;
+            }
+        }
+
+        reader.Close();
+        client.Close();
+        tcp_Listener.Stop();
+    }
+}
