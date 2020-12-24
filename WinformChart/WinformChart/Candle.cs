e82f39727c4ad0991e27c6166762261f803e57e6
KangsanBae <warimo47@naver.com>
2020-12-24 12:02:59 +0900

    Add WinformChart project

---
+++ WinformChart/WinformChart/Candle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WinformChart
+{
+    class Candle
+    {
+        public string time { get; set; }
+        public int open { get; set; }
+        public int close { get; set; }
+        public int min { get; set; }
+        public int max { get; set; }
+
+        public Candle(int _day)
+        {
+            time = "2020-12-" + (24 - _day).ToString() + "";
+            Random r = new Random(_day);
+            int temp = r.Next(1000, 10000);
+            open = temp + 100;
+            min = temp;
+            close = temp + 200;
+            max = temp + 300;
+        }
+    }
+}
---
+++ WinformChart/WinformChart/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WinformChart
+{
+    public partial class Form1 : Form
+    {
+        private Series chartSeries;
+        private List<Candle> priceInfoList;
+
+        public int max = 100;
+        public int min = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            chartSeries = chart1.Series["Candle"];
+            // chartSeries["PriceUpColor"] = "Red";
+            // chartSeries["PriceDownColor"] = "Blue";
+            // chart1.AxisViewChanged += chart1_AxisViewChanged;
+
+            priceInfoList = new List<Candle>();
+
+            for (int i = 0; i < 10; ++i)
+            {
+                priceInfoList.Add(new Candle(i));
+            }
+
+            for(int i = 0; i < priceInfoList.Count; ++i)
+            {
+                chartSeries.Points.AddXY(priceInfoList[i].time, priceInfoList[i].max);
+                chartSeries.Points[i].YValues[1] = priceInfoList[i].min;
+                chartSeries.Points[i].YValues[2] = priceInfoList[i].open;
+                chartSeries.Points[i].YValues[3] = priceInfoList[i].close;
+            }
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_AxisViewChanged(object sender, ViewEventArgs e)
+        {
+            if (sender.Equals(chart1) == false) return;
+            
+            int startPosition = (int)e.Axis.ScaleView.ViewMinimum;
+            int endPosition = (int)e.Axis.ScaleView.ViewMaximum;
+
+            int max = (int)e.ChartArea.AxisY.ScaleView.ViewMinimum;
+            int min = (int)e.ChartArea.AxisY.ScaleView.ViewMaximum;
+
+            for (int i = startPosition - 1; i < endPosition; ++i)
+            {
+                if (i < 0) i = 0;
+                if (i >= priceInfoList.Count) break;
+                if (priceInfoList[i].max > max) max = priceInfoList[i].max;
+                if (priceInfoList[i].min < min) min = priceInfoList[i].min;
+            }
+
+            chart1.ChartAreas[0].AxisY.Maximum = max;
+            chart1.ChartAreas[0].AxisY.Minimum = min;
+        }
+    }
+}
