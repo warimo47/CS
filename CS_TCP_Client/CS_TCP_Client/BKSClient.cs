9cd2f22e165d119ad5d1938e8cef499c679b57be
KangsanBae <warimo47@naver.com>
2021-05-18 15:00:59 +0900

    interface update

---
+++ CS_TCP_Client/CS_TCP_Client/BKSClient.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+public class BKSClient
+{
+    // ========== TCP Module ==========
+    public TCP_Client myTCP_Module = null;
+
+    // ========== Network ==========
+    public byte[] recvBuffer = new byte[Define.recvBufferSize];
+    public Socket mySocket;
+    public BKSClient(TCP_Client _TCP_Client, Socket _MySocket)
+    {
+        myTCP_Module = _TCP_Client;
+        mySocket = _MySocket;
+    }
+
+    ~BKSClient()
+    {
+        // Debug.Log("Delete() " + mySocket.RemoteEndPoint.ToString());
+    }
+
+    // 아무 패킷이나 받으면 호출 되는 콜벡 함수
+    public void PacketReceived(IAsyncResult ar)
+    {
+        int received = 0;
+
+        try
+        {
+            // 패킷 수신 함수 호출
+            received = mySocket.EndReceive(ar);
+
+            // Debug.Log("EndReceive() called");
+        }
+        catch (Exception ex)
+        {
+            // Debug.Log("EndReceive() " + ex.ToString());
+        }
+
+        // 연결 끊김
+        if (received == 0)
+        {
+            try
+            {
+                mySocket.Close();
+
+                myTCP_Module.DisconnectClient(this);
+            }
+            catch (Exception ex)
+            {
+                // Debug.Log("Close() " + ex.ToString());
+            }
+            return;
+        }
+
+        try
+        {
+            // 패킷 처리 함수 호출
+            ProccessRecv();
+        }
+        catch (Exception ex)
+        {
+            // Debug.Log("ProccessRecv() " + ex.ToString());
+        }
+
+        try
+        {
+            // 다시 비동기 수신 대기
+            mySocket.BeginReceive(recvBuffer, 0, Define.recvBufferSize, 0, PacketReceived, this);
+        }
+        catch (Exception ex)
+        {
+            // Debug.Log("BeginReceive() " + ex.ToString());
+        }
+    }
+
+    // 패킷 처리 함수
+    public unsafe void ProccessRecv()
+    {
+        string debugLine = "[" + mySocket.RemoteEndPoint.ToString();
+        string ipStr = "";
+        string deviceIDStr = "";
+        string timeStr = "";
+        byte[] ipStrBits;
+        byte[] timeStrBits;
+        byte objNum;
+        byte[] deviceIDStrBits;
+        byte deviceStatusInformation;
+        byte[] fourByteBits;
+
+        // 패킷의 종류에 따라 분기
+        switch ((int)recvBuffer[0])
+        {
+            case 1: // IF-SC100.header
+                ipStrBits = new byte[32];
+                Array.Copy(recvBuffer, 1, ipStrBits, 0, 32);
+                ipStr = Encoding.Unicode.GetString(ipStrBits); // IF-SC100.IP
+                ipStr = ipStr.Replace("\0", "");
+
+                timeStrBits = new byte[54];
+                Array.Copy(recvBuffer, 33, timeStrBits, 0, 54);
+                timeStr = Encoding.Unicode.GetString(timeStrBits); // IF-SC100.time
+                timeStr = timeStr.Replace("\0", "");
+
+                objNum = recvBuffer[87]; // IF-SC100.objNum
+
+                debugLine = debugLine + " IF-SC100] " + ipStr + " " + timeStr + " " + objNum + "\n";
+
+                fourByteBits = new byte[4];
+
+                for (int i = 0; i < objNum; ++i)
+                {
+                    debugLine = debugLine + i + " : " + recvBuffer[88 + i * 29] + " "; // IF-SC100.objType
+
+                    Array.Copy(recvBuffer, 88 + i * 29 + 1, fourByteBits, 0, 4);
+                    debugLine = debugLine + BitConverter.ToSingle(fourByteBits, 0) + " "; // IF-SC100.XAxis
+
+                    Array.Copy(recvBuffer, 88 + i * 29 + 5, fourByteBits, 0, 4);
+                    debugLine = debugLine + BitConverter.ToSingle(fourByteBits, 0) + " "; // IF-SC100.ZAxis
+
+                    Array.Copy(recvBuffer, 88 + i * 29 + 9, fourByteBits, 0, 4);
+                    debugLine = debugLine + BitConverter.ToInt32(fourByteBits, 0) + " "; // IF-SC100.objX1
+
+                    Array.Copy(recvBuffer, 88 + i * 29 + 13, fourByteBits, 0, 4);
+                    debugLine = debugLine + BitConverter.ToInt32(fourByteBits, 0) + " "; // IF-SC100.objY1
+
+                    Array.Copy(recvBuffer, 88 + i * 29 + 17, fourByteBits, 0, 4);
+                    debugLine = debugLine + BitConverter.ToInt32(fourByteBits, 0) + " "; // IF-SC100.objX2
+
+                    Array.Copy(recvBuffer, 88 + i * 29 + 21, fourByteBits, 0, 4);
+                    debugLine = debugLine + BitConverter.ToInt32(fourByteBits, 0) + " "; // IF-SC100.objY2
+
+                    Array.Copy(recvBuffer, 88 + i * 29 + 25, fourByteBits, 0, 4);
+                    debugLine = debugLine + BitConverter.ToSingle(fourByteBits, 0) + "\n"; // IF-SC100.percent
+                }
+                break;
+            case 2: // IF-SC200.header
+                deviceIDStrBits = new byte[16];
+                Array.Copy(recvBuffer, 1, deviceIDStrBits, 0, 16);
+                deviceIDStr = Encoding.Unicode.GetString(deviceIDStrBits); // IF-SC200.deviceID
+                deviceIDStr = deviceIDStr.Replace("\0", "");
+
+                timeStrBits = new byte[20];
+                Array.Copy(recvBuffer, 17, timeStrBits, 0, 20);
+                timeStr = Encoding.Unicode.GetString(timeStrBits); // IF-SC200.time
+                timeStr = timeStr.Replace("\0", "");
+
+                byte fireAndSmokeStatusInformation = recvBuffer[37]; // IF-SC200.fireAndSmokeStatusInformation
+
+                deviceStatusInformation = recvBuffer[38]; // IF-SC200.deviceStatusInformation
+
+                debugLine += " IF-SC200] " + ipStr + " " + timeStr + " " +
+                    fireAndSmokeStatusInformation + " " + deviceStatusInformation + "\n";
+                break;
+            case 3: // IF-SC300.header
+                deviceIDStrBits = new byte[16];
+                Array.Copy(recvBuffer, 1, deviceIDStrBits, 0, 16);
+                deviceIDStr = Encoding.Unicode.GetString(deviceIDStrBits); // IF-SC300.deviceID
+                ipStr = ipStr.Replace("\0", "");
+
+                timeStrBits = new byte[20];
+                Array.Copy(recvBuffer, 17, timeStrBits, 0, 20);
+                timeStr = Encoding.Unicode.GetString(timeStrBits); // IF-SC300.time
+                timeStr = timeStr.Replace("\0", "");
+
+                byte eventStatusInformation = recvBuffer[37]; // IF-SC300.eventStatusInformation
+
+                deviceStatusInformation = recvBuffer[38]; // IF-SC300.deviceStatusInformation
+
+                byte emergencySituationInformation = recvBuffer[39]; // IF-SC300.emergencySituationInformation
+
+                debugLine += " IF-SC300] " + ipStr + " " + timeStr + " " +
+                    eventStatusInformation + " " + deviceStatusInformation + " " + emergencySituationInformation + "\n";
+                break;
+            case 4: // IF-SC400.header
+                ipStrBits = new byte[32];
+                Array.Copy(recvBuffer, 1, ipStrBits, 0, 32);
+                ipStr = Encoding.Unicode.GetString(ipStrBits); // IF-SC400.IP
+                ipStr = ipStr.Replace("\0", "");
+
+                timeStrBits = new byte[54];
+                Array.Copy(recvBuffer, 33, timeStrBits, 0, 54);
+                timeStr = Encoding.Unicode.GetString(timeStrBits); // IF-SC400.time
+                timeStr = timeStr.Replace("\0", "");
+
+                objNum = recvBuffer[87]; // IF-SC400.objNum
+
+                debugLine = debugLine + " IF-SC400] " + ipStr + " " + timeStr + " " + objNum + "\n";
+
+                fourByteBits = new byte[4];
+
+                for (int i = 0; i < objNum; ++i)
+                {
+                    debugLine = debugLine + i + " : " + recvBuffer[88 + i * 34] + " "; // IF-SC400.objType
+
+                    Array.Copy(recvBuffer, 88 + i * 34 + 1, fourByteBits, 0, 4);
+                    debugLine = debugLine + BitConverter.ToSingle(fourByteBits, 0) + " "; // IF-SC400.XAxis
+
+                    Array.Copy(recvBuffer, 88 + i * 34 + 5, fourByteBits, 0, 4);
+                    debugLine = debugLine + BitConverter.ToSingle(fourByteBits, 0) + " "; // IF-SC400.ZAxis
+
+                    Array.Copy(recvBuffer, 88 + i * 34 + 9, fourByteBits, 0, 4);
+                    debugLine = debugLine + BitConverter.ToInt32(fourByteBits, 0) + " "; // IF-SC400.objX1
+
+                    Array.Copy(recvBuffer, 88 + i * 34 + 13, fourByteBits, 0, 4);
+                    debugLine = debugLine + BitConverter.ToInt32(fourByteBits, 0) + " "; // IF-SC400.objY1
+
+                    Array.Copy(recvBuffer, 88 + i * 34 + 17, fourByteBits, 0, 4);
+                    debugLine = debugLine + BitConverter.ToInt32(fourByteBits, 0) + " "; // IF-SC400.objX2
+
+                    Array.Copy(recvBuffer, 88 + i * 34 + 21, fourByteBits, 0, 4);
+                    debugLine = debugLine + BitConverter.ToInt32(fourByteBits, 0) + " "; // IF-SC400.objY2
+
+                    Array.Copy(recvBuffer, 88 + i * 34 + 25, fourByteBits, 0, 4);
+                    debugLine = debugLine + BitConverter.ToSingle(fourByteBits, 0) + " "; // IF-SC400.percent
+
+                    debugLine = debugLine + recvBuffer[88 + i * 34 + 29] + " "; // IF-SC400.action
+
+                    debugLine = debugLine + recvBuffer[88 + i * 34 + 30] + " "; // IF-SC400.color
+
+                    debugLine = debugLine + recvBuffer[88 + i * 34 + 31] + " "; // IF-SC400.Red
+                    debugLine = debugLine + recvBuffer[88 + i * 34 + 32] + " "; // IF-SC400.Green
+                    debugLine = debugLine + recvBuffer[88 + i * 34 + 33] + "\n"; // IF-SC400.Blue
+                }
+                break;
+            case 5: // IF-SC500.header
+                ipStrBits = new byte[32];
+                Array.Copy(recvBuffer, 1, ipStrBits, 0, 32);
+                ipStr = Encoding.Unicode.GetString(ipStrBits); // IF-SC500.IP
+                ipStr = ipStr.Replace("\0", "");
+
+                timeStrBits = new byte[54];
+                Array.Copy(recvBuffer, 33, timeStrBits, 0, 54);
+                timeStr = Encoding.Unicode.GetString(timeStrBits); // IF-SC500.time
+                timeStr = timeStr.Replace("\0", "");
+
+                objNum = recvBuffer[87]; // IF-SC500.objNum
+
+                debugLine = debugLine + " IF-SC500] " + ipStr + " " + timeStr + " " + objNum + "\n";
+
+                fourByteBits = new byte[4];
+
+                for (int i = 0; i < objNum; ++i)
+                {
+                    debugLine = debugLine + i + " : " + recvBuffer[88 + i * 29] + " "; // IF-SC500.objType
+
+                    Array.Copy(recvBuffer, 88 + i * 29 + 1, fourByteBits, 0, 4);
+                    debugLine = debugLine + BitConverter.ToSingle(fourByteBits, 0) + " "; // IF-SC500.XAxis
+
+                    Array.Copy(recvBuffer, 88 + i * 29 + 5, fourByteBits, 0, 4);
+                    debugLine = debugLine + BitConverter.ToSingle(fourByteBits, 0) + " "; // IF-SC500.ZAxis
+
+                    Array.Copy(recvBuffer, 88 + i * 29 + 9, fourByteBits, 0, 4);
+                    debugLine = debugLine + BitConverter.ToInt32(fourByteBits, 0) + " "; // IF-SC500.objX1
+
+                    Array.Copy(recvBuffer, 88 + i * 29 + 13, fourByteBits, 0, 4);
+                    debugLine = debugLine + BitConverter.ToInt32(fourByteBits, 0) + " "; // IF-SC500.objY1
+
+                    Array.Copy(recvBuffer, 88 + i * 29 + 17, fourByteBits, 0, 4);
+                    debugLine = debugLine + BitConverter.ToInt32(fourByteBits, 0) + " "; // IF-SC500.objX2
+
+                    Array.Copy(recvBuffer, 88 + i * 29 + 21, fourByteBits, 0, 4);
+                    debugLine = debugLine + BitConverter.ToInt32(fourByteBits, 0) + " "; // IF-SC500.objY2
+
+                    Array.Copy(recvBuffer, 88 + i * 29 + 25, fourByteBits, 0, 4);
+                    debugLine = debugLine + BitConverter.ToSingle(fourByteBits, 0) + "\n"; // IF-SC500.percent
+                }
+                break;
+            default:
+                // Debug.Log("recv wrong data " + recvBuffer[0]);
+                break;
+        }
+
+        // 로그 출력 할 것이 있으면 큐에 넣음
+        // scrollController.logQueue.Enqueue((myScroll, myTextParent, debugLine));
+    }
+}
+
--- CS_TCP_Client/CS_TCP_Client/Program.cs
+++ CS_TCP_Client/CS_TCP_Client/Program.cs
@@ -7,48 +7,104 @@ using System.Net.Sockets;
 using System.IO;
 using System.Threading;
 
+static class Define
+{
+    public const int recvBufferSize = 8192;
+    public const int portNum = 4211;
+}
+
 public class TCP_Client
 {
+    public Socket mainSock = null;
+    public Socket mySocket;
+    public List<BKSClient> myClientsList = null;
+
     public static void Main(string[] arg)
     {
-        string args0 = "127.0.0.1"; // ip
-        string args1 = "4211"; // 포트 번호
-        string args2 = "Hello"; // 메시지
+        // 소켓 생성
+        Socket mainSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
 
-        TcpClient client = new TcpClient(); // (ip주소 , 포트 번호)
+        // 클라이언트 리스트 초기화
+        List<BKSClient> myClientsList = new List<BKSClient>();
 
-        while(true)
+        // 소켓 오픈
+        IPEndPoint serverEP = new IPEndPoint(IPAddress.Any, Define.portNum);
+
+        // 소켓 바인드
+        mainSock.Bind(serverEP);
+
+        // 소켓 리슨
+        mainSock.Listen(10); // Why 10 ???
+
+        while (true)
         {
             try
             {
-                client.Connect(args0, Int32.Parse(args1));
-                Console.WriteLine("서버에 연결되었습니다.");
-                break;
+                // Accept 시작
+                mainSock.BeginAccept(this.ClinetAccept, null);
             }
-            catch
+            catch (Exception ex)
             {
-                Console.WriteLine("서버에 연결이 되지 않습니다. 재시도 중...");
+                // Debug.Log("FirstBeginAccept() " + ex.ToString());
             }
         }
-            
-        NetworkStream ns = client.GetStream();
-        StreamWriter writer = new StreamWriter(ns);
+    }
 
-        while(true)
+    public static void ClinetAccept(IAsyncResult ar)
+    {
+        Socket acceptedSocket = null;
+        BKSClient newClient = null;
+
+        try
         {
-            Console.Write("보낼 문자열 입력 : ");
-            args2 = Console.ReadLine();
+            // 클라이언트의 연결 요청 수락
+            acceptedSocket = mainSock.EndAccept(ar);
+        }
+        catch (Exception ex)
+        {
+            // Debug.Log("EndAccept() " + ex.ToString());
+        }
 
+        try
+        {
+            // 또 다른 클라이언트의 연결 대기
+            mainSock.BeginAccept(ClinetAccept, null);
+        }
+        catch (Exception ex)
+        {
+            // Debug.Log("BeginAccept() " + ex.ToString());
+        }
+
+        try
+        {
+            // 새로운 클라이언트 객체 생성
+            newClient = new BKSClient(this, acceptedSocket); ;
+
+            // 새로운 클라이언트 객체 배열에 추가
+            myClientsList.Add(newClient);
+        }
+        catch (Exception ex)
+        {
+            // Debug.Log("new MyClient() " + ex.ToString());
+        }
+
+        if (newClient != null)
+        {
             try
             {
-                writer.WriteLine(args2);
-                writer.Flush();
+                // 비동기적 Recv 시작
+                acceptedSocket.BeginReceive(newClient.recvBuffer, 0, Define.recvBufferSize, 0, newClient.PacketReceived, newClient);
             }
-            catch
+            catch (Exception ex)
             {
-                Console.WriteLine("서버와 연결이 끊어졌습니다.");
-                break;
+                // Debug.Log("BeginReceive() " + ex.ToString());
             }
         }
+    }
+
+    public void DisconnectClient(BKSClient _localBKSClient)
+    {
+        // 클라이언트 리스트에서 연결이 끊어진 클라이언트 제거
+        myClientsList.Remove(_localBKSClient);
     }
 }
